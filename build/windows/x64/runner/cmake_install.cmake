1300649472ef4ce524e4637047aa84346a8a621a
DorlixitoG <vbkevin.iesanpablo@gmail.com>
2024-04-07 15:51:00 -0500

    dependecias cacorras

--- build/windows/x64/runner/cmake_install.cmake
+++ build/windows/x64/runner/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Cristian/Desktop/proyecto_moviles/windows/runner
+# Install script for directory: C:/Users/Yesica Herrera/Desktop/ProyectoMoviles/windows/runner
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
